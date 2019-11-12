284a117084a0233460ebef0fa078f84cbf18ec73
kderme <k.dermenz@gmail.com>
2019-11-12 16:22:07 +0200

    Remove unit test

--- ouroboros-consensus/test-storage/Test/Ouroboros/Storage/FS.hs
+++ ouroboros-consensus/test-storage/Test/Ouroboros/Storage/FS.hs
@@ -3,6 +3,8 @@
 
 module Test.Ouroboros.Storage.FS
   ( tests
+  -- exported to silence warnings.
+  , test_example
   ) where
 
 import qualified Test.Ouroboros.Storage.FS.StateMachine as StateMachine
@@ -11,21 +13,18 @@ import           Test.Tasty (TestTree, testGroup)
 import           Test.Tasty.HUnit
 
 import           Ouroboros.Storage.FS.API
-import           Ouroboros.Storage.FS.API.Types
 
 tests :: FilePath -> TestTree
 tests tmpDir = testGroup "HasFS"
     [ StateMachine.tests tmpDir
-      -- hOpen
-    , testCase "hOpen ReadOnly and hPut" test_hOpenWriteInvalid
     ]
 
 {------------------------------------------------------------------------------
  The tests proper
 -------------------------------------------------------------------------------}
 
-test_hOpenWriteInvalid :: Assertion
-test_hOpenWriteInvalid = apiEquivalenceFs (expectFsError FsInvalidArgument) $ \hasFS@HasFS{..} _err -> do
-    _  <- hOpen (mkFsPath ["foo.txt"]) (WriteMode MustBeNew)
-    h2 <- hOpen (mkFsPath ["foo.txt"]) ReadMode
-    hPut hasFS h2 "haskell-is-nice"
+-- | A unit test example.
+test_example :: Assertion
+test_example =
+  apiEquivalenceFs (expectFsResult (@?= ())) $ \HasFS{..} _err ->
+    return ()
